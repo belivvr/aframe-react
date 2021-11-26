4eab67d14f40fd36826b34ea30c1f0895f6db831
juunini <juuni.ni.i@gmail.com>
2021-11-27 00:43:30 +0900

    Add background in index

--- src/components/scene/index.ts
+++ src/components/scene/index.ts
@@ -1,5 +1,7 @@
 export type { FogType, FogProps } from './fog';
 export type { InspectorProps } from './inspector';
+export type { BackgroundProps } from './background';
 
 export { Fog } from './fog';
 export { Inspector } from './inspector';
+export { Background } from './background';

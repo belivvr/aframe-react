48b12bf8648400802af7b84b8bf8d22a5d087e91
juunini <juuni.ni.i@gmail.com>
2021-11-26 14:41:38 +0900

    Write Scene react component

---
+++ src/core/Scene.tsx
@@ -0,0 +1,33 @@
+import type {
+  FogProps,
+  InspectorProps,
+} from '../components/scene';
+import {
+  Fog,
+  Inspector,
+} from '../components/scene';
+
+interface Props {
+  fog?: FogProps;
+  inspector?: InspectorProps;
+  children?: React.ReactNode;
+  registeredComponents: {
+    [key: string]: string | number | boolean;
+  };
+}
+
+export default function Scene({
+  fog = {},
+  inspector = {},
+  children,
+}: Props): JSX.Element {
+  return (
+    <a-scene
+      fog={new Fog(fog).toString()}
+      inspector={new Inspector(inspector).toString()}
+      {...registeredComponents}
+    >
+      {children}
+    </a-scene>
+  );
+}

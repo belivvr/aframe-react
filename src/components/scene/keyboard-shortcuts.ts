b72d5646576aa3f601f4af735109e19f4a92d743
juunini <juuni.ni.i@gmail.com>
2021-11-27 16:04:56 +0900

    Add exitVR property in keyboard-shortcuts

--- src/components/scene/keyboard-shortcuts.ts
+++ src/components/scene/keyboard-shortcuts.ts
@@ -7,16 +7,26 @@ export interface KeyboardShortcutsProps {
    * Enables the shortcut to press ‘F’ to enter VR.
    */
   enterVR?: boolean;
+
+  /**
+   * Enables the shortcut to press ‘ESC’ to exit VR.
+   */
+  exitVR?: boolean;
 }
 
 export class KeyboardShortcuts implements KeyboardShortcutsProps {
   readonly enterVR: boolean;
 
+  readonly exitVR: boolean;
+
   constructor({
     enterVR = true,
+    exitVR = true,
   }: KeyboardShortcutsProps) {
     this.enterVR = enterVR;
+    this.exitVR = exitVR;
   }
 
-  public toString = (): string => `enterVR:${this.enterVR};`;
+  public toString = (): string => `enterVR:${this.enterVR};`
+                                + `exitVR:${this.exitVR};`;
 }

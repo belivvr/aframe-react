ddb913c7c87ef428546449ec325e30ae3badf136
juunini <juuni.ni.i@gmail.com>
2021-11-30 13:53:19 +0900

    Write windows-motion-controls component

---
+++ src/components/windows-motion-controls.ts
@@ -0,0 +1,56 @@
+import type { Hand } from './types';
+
+/**
+ * The windows-motion-controls component interfaces with any spatial controllers exposed through Windows Mixed Reality as Spatial Input Sources (such as Motion Controllers). It wraps the [tracked-controls component](https://aframe.io/docs/1.2.0/components/tracked-controls.html) while adding button mappings, events, and a controller model that highlights applies position/rotation transforms to the pressed buttons (trigger, grip, menu, thumbstick, trackpad) and moved axes (thumbstick and trackpad.)
+ *
+ * @see https://aframe.io/docs/1.2.0/components/windows-motion-controls.html
+ */
+export interface WindowsMotionControlsProps {
+  /**
+   * The hand that will be tracked (i.e., right, left).
+   */
+  hand?: Hand;
+
+  /**
+   * Which pair of controllers, if > 2 are connected.
+   */
+  pair?: number;
+
+  /**
+   * Whether the controller model is loaded.
+   */
+  model?: boolean;
+
+  /**
+   * Disable rendering of controller model
+   * when no matching gamepad (based on ID & hand) is connected.
+   */
+  hideDisconnected?: boolean;
+}
+
+export class WindowsMotionControls implements WindowsMotionControlsProps {
+  readonly hand: Hand;
+
+  readonly pair: number;
+
+  readonly model: boolean;
+
+  readonly hideDisconnected: boolean;
+
+  constructor({
+    hand = 'right',
+    pair = 0,
+    model = true,
+    hideDisconnected = true,
+  }: WindowsMotionControlsProps) {
+    this.hand = hand;
+    this.pair = pair;
+    this.model = model;
+    this.hideDisconnected = hideDisconnected;
+  }
+
+  public toString = (): string => `hand:${this.hand};`
+                                + `pair:${this.pair};`
+                                + `model:${this.model};`
+                                + `hideDisconnected:${this.hideDisconnected};`;
+}

554ffb9d8660d4e3c0bf2c56635b73e26dd65136
juunini <juuni.ni.i@gmail.com>
2021-11-30 23:54:47 +0900

    Write a-cursor

--- src/extras/primitives/a-cursor.tsx
+++ src/extras/primitives/a-cursor.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-export default function Cursor(): JSX.Element {
-  return <></>;
+interface Props {
+  far?: number;
+  fuse?: boolean;
+  fuseTimeout?: number;
+  interval?: number;
+  objects?: number;
+}
+
+export default function Cursor({
+  far,
+  fuse,
+  fuseTimeout,
+  interval,
+  objects,
+}: Props): JSX.Element {
+  return (
+    <a-cursor
+      far={far}
+      fuse={fuse}
+      fuse-timeout={fuseTimeout}
+      interval={interval}
+      objects={objects}
+    />
+  );
 }

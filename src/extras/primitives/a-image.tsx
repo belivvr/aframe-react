330984c4bd2847bdf0775d0d1e218e74b5e04d11
juunini <juuni.ni.i@gmail.com>
2021-12-01 09:50:51 +0900

    Mocking a-image

--- src/extras/primitives/a-image.tsx
+++ src/extras/primitives/a-image.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
 
-export default function Image(): JSX.Element {
-  return <></>;
+interface Props {
+  color?: unknown;
+  height?: unknown;
+  metalness?: unknown;
+  opacity?: unknown;
+  repeat?: unknown;
+  roughness?: unknown;
+  segmentsHeight?: unknown;
+  segmentsWidth?: unknown;
+  shader?: unknown;
+  side?: unknown;
+  src?: unknown;
+  transparent?: unknown;
+  width?: unknown;
+}
+
+export default function Image({
+  color,
+  height,
+  metalness,
+  opacity,
+  repeat,
+  roughness,
+  segmentsHeight,
+  segmentsWidth,
+  shader,
+  side,
+  src,
+  transparent,
+  width,
+}: Props): JSX.Element {
+  return (
+    <a-image
+      color={color}
+      height={height}
+      metalness={metalness}
+      opacity={opacity}
+      repeat={repeat}
+      roughness={roughness}
+      segments-height={segmentsHeight}
+      segments-width={segmentsWidth}
+      shader={shader}
+      side={side}
+      src={src}
+      transparent={transparent}
+      width={width}
+    />
+  );
 }

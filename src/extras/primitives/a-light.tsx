2d043e1d845c06c5dba224bd64276151f2330f48
juunini <juuni.ni.i@gmail.com>
2021-12-01 09:52:04 +0900

    Mocking a-light

--- src/extras/primitives/a-light.tsx
+++ src/extras/primitives/a-light.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
 
-export default function Light(): JSX.Element {
-  return <></>;
+interface Props {
+  angle?: unknown;
+  color?: unknown;
+  decay?: unknown;
+  distance?: unknown;
+  groundColor?: unknown;
+  intensity?: unknown;
+  penumbra?: unknown;
+  type?: unknown;
+  target?: unknown;
+}
+
+export default function Light({
+  angle,
+  color,
+  decay,
+  distance,
+  groundColor,
+  intensity,
+  penumbra,
+  type,
+  target,
+}: Props): JSX.Element {
+  return (
+    <a-light
+      angle={angle}
+      color={color}
+      decay={decay}
+      distance={distance}
+      ground-color={groundColor}
+      intensity={intensity}
+      penumbra={penumbra}
+      type={type}
+      target={target}
+    />
+  );
 }

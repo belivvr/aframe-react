646b39a8d8ef2d8ba4cbb7c41cc19d20cb529bbd
juunini <juuni.ni.i@gmail.com>
2021-12-01 19:27:04 +0900

    Mocking a-videosphere

--- src/extras/primitives/a-videosphere.tsx
+++ src/extras/primitives/a-videosphere.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-export default function VideoSphere(): JSX.Element {
-  return <></>;
+interface Props {
+  autoplay?: unknown;
+  crossOrigin?: unknown;
+  loop?: unknown;
+  radius?: unknown;
+  segmentsHeight?: unknown;
+  segmentsWidth?: unknown;
+}
+
+export default function VideoSphere({
+  autoplay,
+  crossOrigin,
+  loop,
+  radius,
+  segmentsHeight,
+  segmentsWidth,
+}: Props): JSX.Element {
+  return (
+    <a-videosphere
+      autoplay={autoplay}
+      // eslint-disable-next-line react/no-unknown-property
+      crossOrigin={crossOrigin}
+      loop={loop}
+      radius={radius}
+      segments-height={segmentsHeight}
+      segments-width={segmentsWidth}
+    />
+  );
 }

eebc296127b65b395fd1fa6a796346a54d7f93d8
juunini <juuni.ni.i@gmail.com>
2021-12-01 09:41:35 +0900

    Write a-curvedimage

--- src/extras/primitives/a-curvedimage.tsx
+++ src/extras/primitives/a-curvedimage.tsx
@@ -1,5 +1,62 @@
 import React from 'react';
 
-export default function CurvedImage(): JSX.Element {
-  return <></>;
+import type {
+  Color,
+  Metalness,
+  Repeat,
+  Roughness,
+  Src,
+} from '../material';
+import type { Cylinder as CylinderGeometry } from '../geometry';
+
+type Props = CylinderGeometry & {
+  color?: Color;
+  metalness?: Metalness;
+  opacity?: number;
+  repeat?: Repeat;
+  roughness?: Roughness;
+  shader?: string;
+  side?: string;
+  src?: Src;
+  transparent?: boolean;
+};
+
+export default function CurvedImage({
+  color,
+  metalness,
+  opacity,
+  repeat,
+  roughness,
+  shader,
+  side,
+  src,
+  transparent,
+  height,
+  openEnded,
+  radius,
+  segmentsHeight,
+  segmentsRadial,
+  thetaLength,
+  thetaStart,
+}: Props): JSX.Element {
+  return (
+    <a-curvedimage
+      color={color}
+      height={height}
+      metalness={metalness}
+      opacity={opacity}
+      open-ended={openEnded}
+      radius={radius}
+      repeat={repeat}
+      roughness={roughness}
+      segments-height={segmentsHeight}
+      segments-radial={segmentsRadial}
+      shader={shader}
+      side={side}
+      src={src}
+      theta-length={thetaLength}
+      theta-start={thetaStart}
+      transparent={transparent}
+    />
+  );
 }

27f5e02e78fa13add1b2c8ed3e3f8e7f02cd69a3
juunini <juuni.ni.i@gmail.com>
2021-12-01 18:45:05 +0900

    Mocking a-sound

--- src/extras/primitives/a-sound.tsx
+++ src/extras/primitives/a-sound.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-export default function Sound(): JSX.Element {
-  return <></>;
+interface Props {
+  autoplay?: unknown;
+  loop?: unknown;
+  on?: unknown;
+  src?: unknown;
+  volume?: unknown;
+}
+
+export default function Sound({
+  autoplay,
+  loop,
+  on,
+  src,
+  volume,
+}: Props): JSX.Element {
+  return (
+    <a-sound
+      autoplay={autoplay}
+      loop={loop}
+      on={on}
+      src={src}
+      volume={volume}
+    />
+  );
 }

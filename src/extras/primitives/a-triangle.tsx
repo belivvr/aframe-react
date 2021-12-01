ba09fbca3402d87ec2ae4e84b7f7805b2103cca8
juunini <juuni.ni.i@gmail.com>
2021-12-01 19:20:51 +0900

    Mocking a-triangle

--- src/extras/primitives/a-triangle.tsx
+++ src/extras/primitives/a-triangle.tsx
@@ -1,5 +1,96 @@
 import React from 'react';
 
-export default function Triangle(): JSX.Element {
-  return <></>;
+interface Props {
+  ambientOcclusionMap?: unknown;
+  ambientOcclusionMapIntensity?: unknown;
+  ambientOcclusionTextureOffset?: unknown;
+  ambientOcclusionTextureRepeat?: unknown;
+  color?: unknown;
+  displacementBias?: unknown;
+  displacementMap?: unknown;
+  displacementScale?: unknown;
+  displacementTextureOffset?: unknown;
+  displacementTextureRepeat?: unknown;
+  envMap?: unknown;
+  fog?: unknown;
+  height?: unknown;
+  metalness?: unknown;
+  normalMap?: unknown;
+  normalScale?: unknown;
+  normalTextureOffset?: unknown;
+  normalTextureRepeat?: unknown;
+  repeat?: unknown;
+  roughness?: unknown;
+  sphericalEnvMap?: unknown;
+  src?: unknown;
+  vertexA?: unknown;
+  vertexB?: unknown;
+  vertexC?: unknown;
+  width?: unknown;
+  wireframe?: unknown;
+  wireframeLinewidth?: unknown;
+}
+
+export default function Triangle({
+  ambientOcclusionMap,
+  ambientOcclusionMapIntensity,
+  ambientOcclusionTextureOffset,
+  ambientOcclusionTextureRepeat,
+  color,
+  displacementBias,
+  displacementMap,
+  displacementScale,
+  displacementTextureOffset,
+  displacementTextureRepeat,
+  envMap,
+  fog,
+  height,
+  metalness,
+  normalMap,
+  normalScale,
+  normalTextureOffset,
+  normalTextureRepeat,
+  repeat,
+  roughness,
+  sphericalEnvMap,
+  src,
+  vertexA,
+  vertexB,
+  vertexC,
+  width,
+  wireframe,
+  wireframeLinewidth,
+}: Props): JSX.Element {
+  return (
+    <a-triangle
+      ambient-occlusion-map={ambientOcclusionMap}
+      ambient-occlusion-map-intensity={ambientOcclusionMapIntensity}
+      ambient-occlusion-texture-offset={ambientOcclusionTextureOffset}
+      ambient-occlusion-texture-repeat={ambientOcclusionTextureRepeat}
+      color={color}
+      displacement-bias={displacementBias}
+      displacement-map={displacementMap}
+      displacement-scale={displacementScale}
+      displacement-texture-offset={displacementTextureOffset}
+      displacement-texture-repeat={displacementTextureRepeat}
+      env-map={envMap}
+      fog={fog}
+      height={height}
+      metalness={metalness}
+      normal-map={normalMap}
+      normal-scale={normalScale}
+      normal-texture-offset={normalTextureOffset}
+      normal-texture-repeat={normalTextureRepeat}
+      repeat={repeat}
+      roughness={roughness}
+      spherical-env-map={sphericalEnvMap}
+      src={src}
+      vertex-a={vertexA}
+      vertex-b={vertexB}
+      vertex-c={vertexC}
+      width={width}
+      wireframe={wireframe}
+      wireframe-linewidth={wireframeLinewidth}
+    />
+  );
 }

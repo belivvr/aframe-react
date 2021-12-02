42c44c68c44cf0effa372df03849b46403d7ba15
juunini <juuni.ni.i@gmail.com>
2021-12-02 22:14:55 +0900

    Add core items in index

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,5 @@
 export { default as Scene } from './Scene';
 export { default as Entity } from './Entity';
+export { default as Mixin } from './Mixin';
+export { default as Assets } from './Assets';
+export { default as AssetItem } from './AssetItem';

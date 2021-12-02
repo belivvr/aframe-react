9418071649cb0541196d41c0eed9b940cd3530d6
juunini <juuni.ni.i@gmail.com>
2021-12-03 00:12:10 +0900

    Fix a-box props type

--- src/extras/primitives/a-box.tsx
+++ src/extras/primitives/a-box.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
 
+import type { Vec2 } from '../../components';
+import { Vec2ToString } from '../../components';
 import { EntityProps, toProps } from '../../core';
 
 interface Props {
-  ambientOcclusionMap?: unknown;
-  ambientOcclusionMapIntensity?: unknown;
-  ambientOcclusionTextureOffset?: unknown;
-  ambientOcclusionTextureRepeat?: unknown;
-  color?: unknown;
-  depth?: unknown;
-  displacementBias?: unknown;
-  displacementMap?: unknown;
-  displacementScale?: unknown;
-  displacementTextureOffset?: unknown;
-  displacementTextureRepeat?: unknown;
-  envMap?: unknown;
-  fog?: unknown;
-  height?: unknown;
-  metalness?: unknown;
-  normalMap?: unknown;
-  normalScale?: unknown;
-  normalTextureOffset?: unknown;
-  normalTextureRepeat?: unknown;
-  repeat?: unknown;
-  roughness?: unknown;
-  segmentsDepth?: unknown;
-  segmentsHeight?: unknown;
-  segmentsWidth?: unknown;
-  sphericalEnvMap?: unknown;
-  src?: unknown;
-  width?: unknown;
-  wireframe?: unknown;
-  wireframeLinewidth?: unknown;
+  ambientOcclusionMap?: string;
+  ambientOcclusionMapIntensity?: number;
+  ambientOcclusionTextureOffset?: Vec2;
+  ambientOcclusionTextureRepeat?: Vec2;
+  color?: string;
+  depth?: number;
+  displacementBias?: number;
+  displacementMap?: string;
+  displacementScale?: number;
+  displacementTextureOffset?: Vec2;
+  displacementTextureRepeat?: Vec2;
+  envMap?: string;
+  fog?: boolean;
+  height?: number;
+  metalness?: number;
+  normalMap?: string;
+  normalScale?: Vec2;
+  normalTextureOffset?: Vec2;
+  normalTextureRepeat?: Vec2;
+  repeat?: Vec2;
+  roughness?: number;
+  segmentsDepth?: number;
+  segmentsHeight?: number;
+  segmentsWidth?: number;
+  sphericalEnvMap?: string;
+  src?: string;
+  width?: number;
+  wireframe?: boolean;
+  wireframeLinewidth?: number;
 }
 
 function toBoxProps({
@@ -68,24 +70,24 @@ function toBoxProps({
   return {
     'ambient-occlusion-map': ambientOcclusionMap,
     'ambient-occlusion-map-intensity': ambientOcclusionMapIntensity,
-    'ambient-occlusion-texture-offset': ambientOcclusionTextureOffset,
-    'ambient-occlusion-texture-repeat': ambientOcclusionTextureRepeat,
+    'ambient-occlusion-texture-offset': ambientOcclusionTextureOffset && Vec2ToString(ambientOcclusionTextureOffset),
+    'ambient-occlusion-texture-repeat': ambientOcclusionTextureRepeat && Vec2ToString(ambientOcclusionTextureRepeat),
     color,
     depth,
     'displacement-bias': displacementBias,
     'displacement-map': displacementMap,
     'displacement-scale': displacementScale,
-    'displacement-texture-offset': displacementTextureOffset,
-    'displacement-texture-repeat': displacementTextureRepeat,
+    'displacement-texture-offset': displacementTextureOffset && Vec2ToString(displacementTextureOffset),
+    'displacement-texture-repeat': displacementTextureRepeat && Vec2ToString(displacementTextureRepeat),
     'env-map': envMap,
     fog,
     height,
     metalness,
     'normal-map': normalMap,
     'normal-scale': normalScale,
-    'normal-texture-offset': normalTextureOffset,
-    'normal-texture-repeat': normalTextureRepeat,
-    repeat,
+    'normal-texture-offset': normalTextureOffset && Vec2ToString(normalTextureOffset),
+    'normal-texture-repeat': normalTextureRepeat && Vec2ToString(normalTextureRepeat),
+    repeat: repeat && Vec2ToString(repeat),
     roughness,
     'segments-depth': segmentsDepth,
     'segments-height': segmentsHeight,
@@ -98,6 +100,25 @@ function toBoxProps({
   };
 }
 
+/**
+ * The box primitive creates shapes such as boxes, cubes, or walls.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-box.html
+ *
+ * @example
+ *
+ * ```tsx
+ * <Assets>
+ *   <img id="texture" src="texture.png" />
+ * </Assets>
+ *
+ * // Basic box.
+ * <Box color="tomato" depth={2} height={4} width={0.5} />
+ *
+ * // Textured box.
+ * <Box src="#texture" />
+ * ```
+ */
 export default function Box(props: Props & EntityProps): JSX.Element {
   const { children } = props;
 

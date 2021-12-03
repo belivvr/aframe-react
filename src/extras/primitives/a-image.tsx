cd84e4b5372745ed715504e7495fe0c930a65eb8
juunini <juuni.ni.i@gmail.com>
2021-12-03 14:09:53 +0900

    Complete a-image

--- src/extras/primitives/a-image.tsx
+++ src/extras/primitives/a-image.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
+import type { MaterialSide, Shader, Vec2Props } from '../../components';
+import type { EntityProps } from '../../core';
+import { Vec2 } from '../../components';
+import { toProps } from '../../core';
+
 interface Props {
-  color?: unknown;
-  height?: unknown;
-  metalness?: unknown;
-  opacity?: unknown;
-  repeat?: unknown;
-  roughness?: unknown;
-  segmentsHeight?: unknown;
-  segmentsWidth?: unknown;
-  shader?: unknown;
-  side?: unknown;
-  src?: unknown;
-  transparent?: unknown;
-  width?: unknown;
+  color?: string;
+  height?: number;
+  metalness?: number;
+  opacity?: number;
+  repeat?: Vec2Props;
+  roughness?: number;
+  segmentsHeight?: number;
+  segmentsWidth?: number;
+  shader?: Shader;
+  side?: MaterialSide;
+  src?: string;
+  transparent?: boolean;
+  width?: number;
 }
 
-export default function Image({
+function toImageProps({
   color,
   height,
   metalness,
@@ -30,22 +35,69 @@ export default function Image({
   src,
   transparent,
   width,
-}: Props): JSX.Element {
+}: Props): Object {
+  return {
+    color,
+    height,
+    metalness,
+    opacity,
+    repeat: repeat && new Vec2(repeat).toString(),
+    roughness,
+    'segments-height': segmentsHeight,
+    'segments-width': segmentsWidth,
+    shader,
+    side,
+    src,
+    transparent,
+    width,
+  };
+}
+
+/**
+ * The image primitive shows an image on a flat plane.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-image.html
+ *
+ * @example
+ * ```tsx
+ * <Scene>
+ *   <Assets>
+ *     <img id="my-image" src="image.png" />
+ *   </Assets>
+ *
+ *   // Using the asset management system.
+ *   <Image src="#my-image" />
+ *
+ *   // Defining the URL inline. Not recommended but more comfortable for web developers.
+ *   <Image src="another-image.png" />
+ * </Scene>
+ * ```
+ */
+export default function Image(props: Props & EntityProps): JSX.Element {
+  const imageKeys = [
+    'color',
+    'height',
+    'metalness',
+    'opacity',
+    'repeat',
+    'roughness',
+    'segmentsHeight',
+    'segmentsWidth',
+    'shader',
+    'side',
+    'src',
+    'transparent',
+    'width',
+  ];
+
+  const { children } = props;
+
   return (
     <a-image
-      color={color}
-      height={height}
-      metalness={metalness}
-      opacity={opacity}
-      repeat={repeat}
-      roughness={roughness}
-      segments-height={segmentsHeight}
-      segments-width={segmentsWidth}
-      shader={shader}
-      side={side}
-      src={src}
-      transparent={transparent}
-      width={width}
-    />
+      {...toProps(props, imageKeys)}
+      {...toImageProps(props)}
+    >
+      {children}
+    </a-image>
   );
 }

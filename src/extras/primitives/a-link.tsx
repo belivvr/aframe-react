7d5de9f4c521ca38679c8ceb64a2c2cb149c80ba
juunini <juuni.ni.i@gmail.com>
2021-12-03 14:16:16 +0900

    Complete a-link

--- src/extras/primitives/a-link.tsx
+++ src/extras/primitives/a-link.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 
+import type { EntityProps } from '../../core';
+import { toProps } from '../../core';
+
 interface Props {
   href?: string;
   title?: string;
   src?: string;
 }
 
-export default function Link({
-  href,
-  title,
-  src,
-}: Props): JSX.Element {
+/**
+ * The link primitive provides a compact API to define links that
+ * resembles the traditional `<a>` tag.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-link.html
+ */
+export default function Link(props: Props & EntityProps): JSX.Element {
+  const {
+    href,
+    title,
+    src,
+    children,
+  } = props;
+
   return (
     <a-link
+      {...toProps(props)}
       href={href}
       title={title}
       src={src}
-    />
+    >
+      {children}
+    </a-link>
   );
 }

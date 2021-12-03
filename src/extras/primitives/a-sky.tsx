5abdc2d5ff4ebd7e7dfc2148ae8e5a670d929fa2
juunini <juuni.ni.i@gmail.com>
2021-12-03 14:49:10 +0900

    Complete a-sky

--- src/extras/primitives/a-sky.tsx
+++ src/extras/primitives/a-sky.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 
+import type { MaterialSide, Shader, Vec2Props } from '../../components';
+import type { EntityProps } from '../../core';
+import { Vec2 } from '../../components';
+import { toProps } from '../../core';
+
 interface Props {
-  color?: unknown;
-  metalness?: unknown;
-  opacity?: unknown;
-  phiLength?: unknown;
-  phiStart?: unknown;
-  radius?: unknown;
-  repeat?: unknown;
-  roughness?: unknown;
-  segmentsHeight?: unknown;
-  segmentsWidth?: unknown;
-  shader?: unknown;
-  side?: unknown;
-  src?: unknown;
-  thetaLength?: unknown;
-  thetaStart?: unknown;
-  transparent?: unknown;
+  color?: string;
+  metalness?: number;
+  opacity?: number;
+  phiLength?: number;
+  phiStart?: number;
+  radius?: number;
+  repeat?: Vec2Props;
+  roughness?: number;
+  segmentsHeight?: number;
+  segmentsWidth?: number;
+  shader?: Shader;
+  side?: MaterialSide;
+  src?: string;
+  thetaLength?: number;
+  thetaStart?: number;
+  transparent?: boolean;
 }
 
-export default function Sky({
+function toSkyProps({
   color,
   metalness,
   opacity,
@@ -36,25 +41,79 @@ export default function Sky({
   thetaLength,
   thetaStart,
   transparent,
-}: Props): JSX.Element {
+}: Props): Object {
+  return {
+    color,
+    metalness,
+    opacity,
+    'phi-length': phiLength,
+    'phi-start': phiStart,
+    radius,
+    repeat: repeat && new Vec2(repeat).toString(),
+    roughness,
+    'segments-height': segmentsHeight,
+    'segments-width': segmentsWidth,
+    shader,
+    side,
+    src,
+    'theta-length': thetaLength,
+    'theta-start': thetaStart,
+    transparent,
+  };
+}
+
+/**
+ * The sky primitive adds a background color or 360° image to a scene.
+ * A sky is a large sphere with a color or texture mapped to the inside.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-sky.html
+ *
+ * @example
+ * An equirectangular image as a background:
+ *
+ * ```tsx
+ * <Scene>
+ *   <Assets>
+ *     <img id="sky" src="sky.png" />
+ *   </Assets>
+ *   <Sky src="#sky" />
+ * </Scene>
+ * ```
+ *
+ * A plain color as a background:
+ *
+ * ```tsx
+ * <Sky color="#6EBAA7" />
+ * ```
+ */
+export default function Sky(props: Props & EntityProps): JSX.Element {
+  const skyKeys = [
+    'color',
+    'metalness',
+    'opacity',
+    'phiLength',
+    'phiStart',
+    'radius',
+    'repeat',
+    'roughness',
+    'segmentsHeight',
+    'segmentsWidth',
+    'shader',
+    'side',
+    'src',
+    'thetaLength',
+    'thetaStart',
+    'transparent',
+  ];
+
+  const { children } = props;
+
   return (
     <a-sky
-      color={color}
-      metalness={metalness}
-      opacity={opacity}
-      phi-length={phiLength}
-      phi-start={phiStart}
-      radius={radius}
-      repeat={repeat}
-      roughness={roughness}
-      segments-height={segmentsHeight}
-      segments-width={segmentsWidth}
-      shader={shader}
-      side={side}
-      src={src}
-      theta-length={thetaLength}
-      theta-start={thetaStart}
-      transparent={transparent}
-    />
+      {...toProps(props, skyKeys)}
+      {...toSkyProps(props)}
+    >
+      {children}
+    </a-sky>
   );
 }

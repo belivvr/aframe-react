9fc21c1c1287a34208be51c59a727c25cbabf4ff
juunini <juuni.ni.i@gmail.com>
2021-12-03 11:45:07 +0900

    Add a-camera jsdoc

--- src/extras/primitives/a-camera.tsx
+++ src/extras/primitives/a-camera.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { EntityProps, toProps } from '../../core';
 
 interface Props {
-  far?: unknown;
-  fov?: unknown;
-  lookControlsEnabled?: unknown;
-  near?: unknown;
-  reverseMouseDrag?: unknown;
-  wasdControlsEnabled?: unknown;
+  far?: number;
+  fov?: number;
+  lookControlsEnabled?: boolean;
+  near?: number;
+  reverseMouseDrag?: boolean;
+  wasdControlsEnabled?: boolean;
 }
 
 function toCameraProps({
@@ -29,6 +29,20 @@ function toCameraProps({
   };
 }
 
+/**
+ * The camera primitive determines what the user sees.
+ * We can change the viewport by modifying the camera entity’s position and rotation.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-camera.html
+ *
+ * @example
+ * ```tsx
+ * <Scene>
+ *   <Box />
+ *   <Camera />
+ * </Scene>
+ * ```
+ */
 export default function Camera(props: Props & EntityProps): JSX.Element {
   const cameraKeys = [
     'far',

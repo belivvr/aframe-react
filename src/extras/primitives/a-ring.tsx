6a875fc7ba8f571b5018ea4e40f6056004ec86a3
juunini <juuni.ni.i@gmail.com>
2021-12-03 14:44:02 +0900

    Complete a-ring

--- src/extras/primitives/a-ring.tsx
+++ src/extras/primitives/a-ring.tsx
@@ -1,40 +1,45 @@
 import React from 'react';
 
+import type { Vec2Props } from '../../components';
+import type { EntityProps } from '../../core';
+import { Vec2 } from '../../components';
+import { toProps } from '../../core';
+
 interface Props {
-  ambientOcclusionMap?: unknown;
-  ambientOcclusionMapIntensity?: unknown;
-  ambientOcclusionTextureOffset?: unknown;
-  ambientOcclusionTextureRepeat?: unknown;
-  color?: unknown;
-  displacementBias?: unknown;
-  displacementMap?: unknown;
-  displacementScale?: unknown;
-  displacementTextureOffset?: unknown;
-  displacementTextureRepeat?: unknown;
-  envMap?: unknown;
-  fog?: unknown;
-  height?: unknown;
-  metalness?: unknown;
-  normalMap?: unknown;
-  normalScale?: unknown;
-  normalTextureOffset?: unknown;
-  normalTextureRepeat?: unknown;
-  radiusInner?: unknown;
-  radiusOuter?: unknown;
-  repeat?: unknown;
-  roughness?: unknown;
-  segmentsPhi?: unknown;
-  segmentsTheta?: unknown;
-  sphericalEnvMap?: unknown;
-  src?: unknown;
-  thetaLength?: unknown;
-  thetaStart?: unknown;
-  width?: unknown;
-  wireframe?: unknown;
-  wireframeLinewidth?: unknown;
+  ambientOcclusionMap?: string;
+  ambientOcclusionMapIntensity?: number;
+  ambientOcclusionTextureOffset?: Vec2Props;
+  ambientOcclusionTextureRepeat?: Vec2Props;
+  color?: string;
+  displacementBias?: number;
+  displacementMap?: string;
+  displacementScale?: number;
+  displacementTextureOffset?: Vec2Props;
+  displacementTextureRepeat?: Vec2Props;
+  envMap?: string;
+  fog?: boolean;
+  height?: number;
+  metalness?: number;
+  normalMap?: string;
+  normalScale?: Vec2Props;
+  normalTextureOffset?: Vec2Props;
+  normalTextureRepeat?: Vec2Props;
+  radiusInner?: number;
+  radiusOuter?: number;
+  repeat?: Vec2Props;
+  roughness?: number;
+  segmentsPhi?: number;
+  segmentsTheta?: number;
+  sphericalEnvMap?: string;
+  src?: string;
+  thetaLength?: number;
+  thetaStart?: number;
+  width?: number;
+  wireframe?: boolean;
+  wireframeLinewidth?: number;
 }
 
-export default function Ring({
+function toRingProps({
   ambientOcclusionMap,
   ambientOcclusionMapIntensity,
   ambientOcclusionTextureOffset,
@@ -66,40 +71,107 @@ export default function Ring({
   width,
   wireframe,
   wireframeLinewidth,
-}: Props): JSX.Element {
+}: Props): Object {
+  return {
+    'ambient-occlusion-map': ambientOcclusionMap,
+    'ambient-occlusion-map-intensity': ambientOcclusionMapIntensity,
+    'ambient-occlusion-texture-offset': ambientOcclusionTextureOffset && new Vec2(ambientOcclusionTextureOffset).toString(),
+    'ambient-occlusion-texture-repeat': ambientOcclusionTextureRepeat && new Vec2(ambientOcclusionTextureRepeat).toString(),
+    color,
+    'displacement-bias': displacementBias,
+    'displacement-map': displacementMap,
+    'displacement-scale': displacementScale,
+    'displacement-texture-offset': displacementTextureOffset && new Vec2(displacementTextureOffset).toString(),
+    'displacement-texture-repeat': displacementTextureRepeat && new Vec2(displacementTextureRepeat).toString(),
+    'env-map': envMap,
+    fog,
+    height,
+    metalness,
+    'normal-map': normalMap,
+    'normal-scale': normalScale && new Vec2(normalScale).toString(),
+    'normal-texture-offset': normalTextureOffset && new Vec2(normalTextureOffset).toString(),
+    'normal-texture-repeat': normalTextureRepeat && new Vec2(normalTextureRepeat).toString(),
+    'radius-inner': radiusInner,
+    'radius-outer': radiusOuter,
+    repeat: repeat && new Vec2(repeat).toString(),
+    roughness,
+    'segments-phi': segmentsPhi,
+    'segments-theta': segmentsTheta,
+    'spherical-env-map': sphericalEnvMap,
+    src,
+    'theta-length': thetaLength,
+    'theta-start': thetaStart,
+    width,
+    wireframe,
+    'wireframe-linewidth': wireframeLinewidth,
+  };
+}
+
+/**
+ * The ring primitive creates a ring or disc shape.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-ring.html
+ *
+ * @example
+ * ```tsx
+ * <Assets>
+ *   <img id="texture" src="texture.png" />
+ * </Assets>
+ *
+ * // Basic ring.
+ * <Ring
+ *   color="teal"
+ *   radius-inner={1}
+ *   radius-outer={2}
+ * />
+ *
+ * // Textured ring.
+ * <Ring src="#texture" />
+ * ```
+ */
+export default function Ring(props: Props & EntityProps): JSX.Element {
+  const ringKeys = [
+    'ambientOcclusionMap',
+    'ambientOcclusionMapIntensity',
+    'ambientOcclusionTextureOffset',
+    'ambientOcclusionTextureRepeat',
+    'color',
+    'displacementBias',
+    'displacementMap',
+    'displacementScale',
+    'displacementTextureOffset',
+    'displacementTextureRepeat',
+    'envMap',
+    'fog',
+    'height',
+    'metalness',
+    'normalMap',
+    'normalScale',
+    'normalTextureOffset',
+    'normalTextureRepeat',
+    'radiusInner',
+    'radiusOuter',
+    'repeat',
+    'roughness',
+    'segmentsPhi',
+    'segmentsTheta',
+    'sphericalEnvMap',
+    'src',
+    'thetaLength',
+    'thetaStart',
+    'width',
+    'wireframe',
+    'wireframeLinewidth',
+  ];
+
+  const { children } = props;
+
   return (
     <a-ring
-      ambient-occlusion-map={ambientOcclusionMap}
-      ambient-occlusion-map-intensity={ambientOcclusionMapIntensity}
-      ambient-occlusion-texture-offset={ambientOcclusionTextureOffset}
-      ambient-occlusion-texture-repeat={ambientOcclusionTextureRepeat}
-      color={color}
-      displacement-bias={displacementBias}
-      displacement-map={displacementMap}
-      displacement-scale={displacementScale}
-      displacement-texture-offset={displacementTextureOffset}
-      displacement-texture-repeat={displacementTextureRepeat}
-      env-map={envMap}
-      fog={fog}
-      height={height}
-      metalness={metalness}
-      normal-map={normalMap}
-      normal-scale={normalScale}
-      normal-texture-offset={normalTextureOffset}
-      normal-texture-repeat={normalTextureRepeat}
-      radius-inner={radiusInner}
-      radius-outer={radiusOuter}
-      repeat={repeat}
-      roughness={roughness}
-      segments-phi={segmentsPhi}
-      segments-theta={segmentsTheta}
-      spherical-env-map={sphericalEnvMap}
-      src={src}
-      theta-length={thetaLength}
-      theta-start={thetaStart}
-      width={width}
-      wireframe={wireframe}
-      wireframe-linewidth={wireframeLinewidth}
-    />
+      {...toProps(props, ringKeys)}
+      {...toRingProps(props)}
+    >
+      {children}
+    </a-ring>
   );
 }

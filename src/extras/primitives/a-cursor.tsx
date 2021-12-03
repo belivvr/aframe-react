3fca743a1442d39365e4595811033a8d7fb6967d
juunini <juuni.ni.i@gmail.com>
2021-12-03 12:12:07 +0900

    Complete a-cursor

--- src/extras/primitives/a-cursor.tsx
+++ src/extras/primitives/a-cursor.tsx
@@ -1,27 +1,67 @@
 import React from 'react';
 
+import type { EntityProps } from '../../core';
+import { toProps } from '../../core';
+
 interface Props {
-  far?: unknown;
-  fuse?: unknown;
-  fuseTimeout?: unknown;
-  interval?: unknown;
-  objects?: unknown;
+  far?: number;
+  fuse?: boolean;
+  fuseTimeout?: number;
+  interval?: number;
+  objects?: number;
 }
 
-export default function Cursor({
+function toCursorProps({
   far,
   fuse,
   fuseTimeout,
   interval,
   objects,
-}: Props): JSX.Element {
+}: Props): Object {
+  return {
+    far,
+    fuse,
+    'fuse-timeout': fuseTimeout,
+    interval,
+    objects,
+  };
+}
+
+/**
+ * The cursor primitive is a reticle that allows for clicking and
+ * basic interactivity with a scene on devices that do not have a hand controller.
+ * The default appearance is a ring geometry. The cursor is usually placed as a child of the camera.
+ *
+ * Read the [cursor component documentation](https://aframe.io/docs/1.2.0/components/cursor.html) for detailed information about how the cursor works and how to use the cursor.
+ *
+ * @see https://aframe.io/docs/1.2.0/primitives/a-cursor.html
+ *
+ * @example
+ * ```tsx
+ * <Scene>
+ *   <Camera>
+ *     <Cursor />
+ *   </Camera>
+ *   <Box />
+ * </Scene>
+ * ```
+ */
+export default function Cursor(props: Props & EntityProps): JSX.Element {
+  const cursorKeys = [
+    'far',
+    'fuse',
+    'fuseTimeout',
+    'interval',
+    'objects',
+  ];
+
+  const { children } = props;
   return (
     <a-cursor
-      far={far}
-      fuse={fuse}
-      fuse-timeout={fuseTimeout}
-      interval={interval}
-      objects={objects}
-    />
+      {...toProps(props, cursorKeys)}
+      {...toCursorProps(props)}
+    >
+      {children}
+    </a-cursor>
   );
 }

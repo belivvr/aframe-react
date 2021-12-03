6da2b6534b711a6f11ce63738867a789e3ac225a
juunini <juuni.ni.i@gmail.com>
2021-12-03 13:55:49 +0900

    Complete a-dodecahedron

--- src/extras/primitives/a-dodecahedron.tsx
+++ src/extras/primitives/a-dodecahedron.tsx
@@ -1,41 +1,46 @@
 import React from 'react';
 
+import type { Vec2Props } from '../../components';
+import type { EntityProps } from '../../core';
+import { Vec2 } from '../../components';
+import { toProps } from '../../core';
+
 interface Props {
-  ambientOcclusionMap?: unknown;
-  ambientOcclusionMapIntensity?: unknown;
-  ambientOcclusionTextureOffset?: unknown;
-  ambientOcclusionTextureRepeat?: unknown;
-  color?: unknown;
-  displacementBias?: unknown;
-  displacementMap?: unknown;
-  displacementScale?: unknown;
-  displacementTextureOffset?: unknown;
-  displacementTextureRepeat?: unknown;
-  envMap?: unknown;
-  fog?: unknown;
-  height?: unknown;
-  metalness?: unknown;
-  normalMap?: unknown;
-  normalScale?: unknown;
-  normalTextureOffset?: unknown;
-  normalTextureRepeat?: unknown;
-  openEnded?: unknown;
-  radiusBottom?: unknown;
-  radiusTop?: unknown;
-  repeat?: unknown;
-  roughness?: unknown;
-  segmentsHeight?: unknown;
-  segmentsRadial?: unknown;
-  sphericalEnvMap?: unknown;
-  src?: unknown;
-  thetaLength?: unknown;
-  thetaStart?: unknown;
-  width?: unknown;
-  wireframe?: unknown;
-  wireframeLinewidth?: unknown;
+  ambientOcclusionMap?: string;
+  ambientOcclusionMapIntensity?: number;
+  ambientOcclusionTextureOffset?: Vec2Props;
+  ambientOcclusionTextureRepeat?: Vec2Props;
+  color?: string;
+  displacementBias?: number;
+  displacementMap?: string;
+  displacementScale?: number;
+  displacementTextureOffset?: Vec2Props;
+  displacementTextureRepeat?: Vec2Props;
+  envMap?: string;
+  fog?: boolean;
+  height?: number;
+  metalness?: number;
+  normalMap?: string;
+  normalScale?: Vec2Props;
+  normalTextureOffset?: Vec2Props;
+  normalTextureRepeat?: Vec2Props;
+  openEnded?: boolean;
+  radiusBottom?: number;
+  radiusTop?: number;
+  repeat?: Vec2Props;
+  roughness?: number;
+  segmentsHeight?: number;
+  segmentsRadial?: number;
+  sphericalEnvMap?: string;
+  src?: string;
+  thetaLength?: number;
+  thetaStart?: number;
+  width?: number;
+  wireframe?: boolean;
+  wireframeLinewidth?: number;
 }
 
-export default function Dodecahedron({
+function toDodecahedronProps({
   ambientOcclusionMap,
   ambientOcclusionMapIntensity,
   ambientOcclusionTextureOffset,
@@ -68,41 +73,98 @@ export default function Dodecahedron({
   width,
   wireframe,
   wireframeLinewidth,
-}: Props): JSX.Element {
+}: Props) {
+  return {
+    'ambient-occlusion-map': ambientOcclusionMap,
+    'ambient-occlusion-map-intensity': ambientOcclusionMapIntensity,
+    'ambient-occlusion-texture-offset': ambientOcclusionTextureOffset && new Vec2(ambientOcclusionTextureOffset).toString(),
+    'ambient-occlusion-texture-repeat': ambientOcclusionTextureRepeat && new Vec2(ambientOcclusionTextureRepeat).toString(),
+    color,
+    'displacement-bias': displacementBias,
+    'displacement-map': displacementMap,
+    'displacement-scale': displacementScale,
+    'displacement-texture-offset': displacementTextureOffset && new Vec2(displacementTextureOffset).toString(),
+    'displacement-texture-repeat': displacementTextureRepeat && new Vec2(displacementTextureRepeat).toString(),
+    'env-map': envMap,
+    fog,
+    height,
+    metalness,
+    'normal-map': normalMap,
+    'normal-scale': normalScale && new Vec2(normalScale).toString(),
+    'normal-texture-offset': normalTextureOffset && new Vec2(normalTextureOffset).toString(),
+    'normal-texture-repeat': normalTextureRepeat && new Vec2(normalTextureRepeat).toString(),
+    'open-ended': openEnded,
+    'radius-bottom': radiusBottom,
+    'radius-top': radiusTop,
+    repeat: repeat && new Vec2(repeat).toString(),
+    roughness,
+    'segments-height': segmentsHeight,
+    'segments-radial': segmentsRadial,
+    'spherical-env-map': sphericalEnvMap,
+    src,
+    'theta-length': thetaLength,
+    'theta-start': thetaStart,
+    width,
+    wireframe,
+    'wireframe-linewidth': wireframeLinewidth,
+  };
+}
+
+/**
+ * @see https://aframe.io/docs/1.2.0/primitives/a-dodecahedron.html
+ *
+ * @example
+ * ```tsx
+ * <Dodecahedron
+ *   color="#FF926B"
+ *   radius={5}
+ * />
+ * ```
+ */
+export default function Dodecahedron(props: Props & EntityProps): JSX.Element {
+  const dodecahedronKeys = [
+    'ambientOcclusionMap',
+    'ambientOcclusionMapIntensity',
+    'ambientOcclusionTextureOffset',
+    'ambientOcclusionTextureRepeat',
+    'color',
+    'displacementBias',
+    'displacementMap',
+    'displacementScale',
+    'displacementTextureOffset',
+    'displacementTextureRepeat',
+    'envMap',
+    'fog',
+    'height',
+    'metalness',
+    'normalMap',
+    'normalScale',
+    'normalTextureOffset',
+    'normalTextureRepeat',
+    'openEnded',
+    'radiusBottom',
+    'radiusTop',
+    'repeat',
+    'roughness',
+    'segmentsHeight',
+    'segmentsRadial',
+    'sphericalEnvMap',
+    'src',
+    'thetaLength',
+    'thetaStart',
+    'width',
+    'wireframe',
+    'wireframeLinewidth',
+  ];
+
+  const { children } = props;
+
   return (
     <a-dodecahedron
-      ambient-occlusion-map={ambientOcclusionMap}
-      ambient-occlusion-map-intensity={ambientOcclusionMapIntensity}
-      ambient-occlusion-texture-offset={ambientOcclusionTextureOffset}
-      ambient-occlusion-texture-repeat={ambientOcclusionTextureRepeat}
-      color={color}
-      displacement-bias={displacementBias}
-      displacement-map={displacementMap}
-      displacement-scale={displacementScale}
-      displacement-texture-offset={displacementTextureOffset}
-      displacement-texture-repeat={displacementTextureRepeat}
-      env-map={envMap}
-      fog={fog}
-      height={height}
-      metalness={metalness}
-      normal-map={normalMap}
-      normal-scale={normalScale}
-      normal-texture-offset={normalTextureOffset}
-      normal-texture-repeat={normalTextureRepeat}
-      open-ended={openEnded}
-      radius-bottom={radiusBottom}
-      radius-top={radiusTop}
-      repeat={repeat}
-      roughness={roughness}
-      segments-height={segmentsHeight}
-      segments-radial={segmentsRadial}
-      spherical-env-map={sphericalEnvMap}
-      src={src}
-      theta-length={thetaLength}
-      theta-start={thetaStart}
-      width={width}
-      wireframe={wireframe}
-      wireframe-linewidth={wireframeLinewidth}
-    />
+      {...toProps(props, dodecahedronKeys)}
+      {...toDodecahedronProps(props)}
+    >
+      {children}
+    </a-dodecahedron>
   );
 }

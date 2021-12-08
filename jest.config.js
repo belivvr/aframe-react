0be2faabdd214b40693875aae1d9ce929d39ca9d
juunini <juuni.ni.i@gmail.com>
2021-12-09 05:17:56 +0900

    Add coverage threshold in jest config

--- jest.config.js
+++ jest.config.js
@@ -62,4 +62,12 @@ module.exports = {
     '.stories.',
     '/__snapshots__/',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
